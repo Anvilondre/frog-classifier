f8a45e82df486f60495e8fba23219bab2394e9c0
Roman Burakov <buracov.r@gmail.com>
2020-09-08 11:52:19 +0300

    Added comments and activation function methods.

--- Model.py
+++ Model.py
@@ -1,6 +1,14 @@
 import numpy as np
 
 
+def relu(x):
+    return np.maximum(0, x)
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
 class Model:
     def __init__(self, layer_dims: [int]):
         self.L = len(layer_dims)  # Number of layers
@@ -16,18 +24,23 @@ class Model:
         pass
 
     def initialize_parameters(self):
+        """ Initialize W, b parameters with Xavier initialization. """
         pass
 
     def propagate_forward(self, X):
+        """" Implements forward propagation and returns AL - vector of predictions for samples in X. """
         pass
 
     def compute_cost(self, AL, Y):
+        """ Computes the cross-entropy cost based on predictions vector AL and true labels Y. """
         pass
 
     def propagate_backward(self, AL, Y, caches):
+        """ Competes gradients for further parameters update. """
         pass
 
     def update_parameters(self, learning_rate):
+        """ Updates the W, b parameters. """
         pass
 
 
