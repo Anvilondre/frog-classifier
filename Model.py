569cdc9166a25ce56802482641ecde8e33793ed4
iskiy <vasyarosada@gmail.com>
2020-09-09 08:56:08 +0300

    Hyperparameters update

--- Model.py
+++ Model.py
@@ -3,13 +3,19 @@ from scipy.io import savemat, loadmat
 from DataSplitter import split_train_test, parse_data
 
 
-def relu(x):
-    return x, np.maximum(0, x)
+def relu(Z):
+    A = np.maximum(0, Z)
+    cache = Z
+    return A, cache
 
 
-def relu_backward(dA, Z):
-    dZ = np.array(dA, copy=True)
+def relu_backward(dA, cache):
+    Z = cache
+    dZ = np.array(dA, copy=True)  # just converting dz to a correct object.
+    # When z <= 0, you should set dz to 0 as well.
     dZ[Z <= 0] = 0
+    assert (dZ.shape == Z.shape)
+
     return dZ
 
 
@@ -17,11 +23,11 @@ def sigmoid(x):
     return x, 1 / (1 + np.exp(-x))
 
 
-def sigmoid_backward(dA, Z):
-    Z = Z.copy()
-    Z = np.array(Z)
+def sigmoid_backward(dA, cache):
+    Z = cache
     s = 1 / (1 + np.exp(-Z))
-    return dA * s * (s - 1)
+    dZ = dA * s * (1 - s)
+    return dZ
 
 
 class Model:
@@ -37,17 +43,18 @@ class Model:
         self.dW = [0] * (self.L - 1)
         self.cost = 0
 
-    def fit(self, folds_X, folds_Y, learning_rate=0.0075, number_iterations=3000):
+    def fit(self, folds_X, folds_Y, learning_rate=0.008, number_iterations=6000):
         """" Trains the model. """
         self.initialize_parameters()
         for i in range(number_iterations):
+            if i % 1500 == 0:
+                learning_rate = learning_rate / 2
             AL = self.propagate_forward(folds_X)
             cost = self.compute_cost(AL, folds_Y)
             self.propagate_backward(AL, folds_Y)
             self.update_parameters(learning_rate)
-            if i % 100 == 0:
+            if i % 5 == 0:
                 print(i, cost)
-
 
     def save_weights(self, file='data/weights/weights.mat'):
         savemat(file, mdict={'W': self.W,
@@ -61,9 +68,8 @@ class Model:
     def initialize_parameters(self):
         """ Initialize W, b parameters with Xavier initialization. """
         for i in range(1, self.L):
-            self.W[i - 1] = np.random.randn(self.layer_dims[i], self.layer_dims[i - 1]) * \
-                            np.sqrt(2 / self.layer_dims[i - 1])
-            self.b[i - 1] = np.zeros((self.layer_dims[i], 1))
+            self.W[i - 1] = np.random.randn(self.layer_dims[i], self.layer_dims[i - 1]) * 0.01
+            self.b[i - 1] = np.zeros((self.layer_dims[i], 1)
 
     def propagate_forward(self, X):
         """" Implements forward propagation and returns AL - vector of predictions for samples in X. """
@@ -85,7 +91,7 @@ class Model:
     def compute_cost(self, AL, Y):
         """ Computes the cross-entropy cost based on predictions vector AL and true labels Y. """
         m = Y.shape[1]
-        cost = - (1 / m) * sum(np.matmul(Y, np.log(AL).T) + np.matmul(1 - Y, np.log(1 - AL).T))
+        cost = (-1 / m) * np.sum(np.multiply(Y, np.log(AL)) + np.multiply(1 - Y, np.log(1 - AL)))
         cost = np.squeeze(cost)
 
         return cost
@@ -93,33 +99,46 @@ class Model:
     def linear_backward(self, dZ, cache):
         A_prev, W, b = cache
         m = A_prev.shape[1]
-        dW = np.matmul(dZ, A_prev.T) / m
-        db = np.sum(dZ, axis=1, keepdims=True) / m
-        dA_prev = np.matmul(W.T, dZ)
+        dW = np.dot(dZ, A_prev.T) * (1 / m)
+        db = np.sum(dZ, axis=1, keepdims=True) * (1 / m)
+        dA_prev = np.dot(W.T, dZ)
+        return dA_prev, dW, db
+
+    def linear_activation_backward(self, dA, cache, activation):
+        linear_cache, activation_cache = cache
+        if activation == "relu":
+            dZ = relu_backward(dA, activation_cache)
+            dA_prev, dW, db = self.linear_backward(dZ, linear_cache)
+        elif activation == "sigmoid":
+            dZ = sigmoid_backward(dA, activation_cache)
+            dA_prev, dW, db = self.linear_backward(dZ, linear_cache)
+
         return dA_prev, dW, db
 
     def propagate_backward(self, AL, Y):
         """ Competes gradients for further parameters update. """
         L = len(self.caches)
         Y = Y.reshape(AL.shape)
-
         dAL = - (np.divide(Y, AL) - np.divide(1 - Y, 1 - AL))
+
         cache = self.caches[L - 1]
-
         self.dA[L - 1], self.dW[L - 1], self.db[L - 1] = self.linear_backward(sigmoid_backward(dAL, cache[1]), cache[0])
-
         for i in reversed(range(L - 1)):
             cache = self.caches[i]
-            self.dA[i], self.dW[i], self.db[i] = self.linear_backward(relu_backward(self.dA[i + 1], cache[1]), cache[0])
+            self.dA[i], self.dW[i], self.db[i] = self.linear_backward(sigmoid_backward(self.dA[i + 1], cache[1]),
+                                                                      cache[0])
 
     def update_parameters(self, learning_rate):
         """ Updates the W, b parameters. """
         for i in range(self.L - 1):
-            self.W[i] -= learning_rate*self.W[i]*self.dW[i]
-            self.b[i] -= learning_rate*self.b[i]*self.db[i]
+            self.W[i] -= learning_rate * self.dW[i]
+            self.b[i] -= learning_rate * self.db[i]
 
 
 if __name__ == '__main__':
     X, Y = parse_data('data/frogs', 'data/toads', save=False)
-    model = Model([len(X[0]), 4, 4, 1])
-    model.fit(X, Y)
+    X = X[:400].T
+    Y = Y[:400]
+    model = Model([len(X), 3, 3, 1])
+    Y = Y.reshape((Y.shape[0], 1))
+    model.fit(X, Y.T)
